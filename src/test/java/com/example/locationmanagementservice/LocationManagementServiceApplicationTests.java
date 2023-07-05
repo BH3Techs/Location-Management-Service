e3730e7123cc1c69654a156a1ed55af155b1d24f
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-07-05 14:15:08 +0200

    First Commit

---
+++ src/test/java/com/example/locationmanagementservice/LocationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locationmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
